ffe130de201d443b25ed247fdb3a4e846d0e70fb
jallen70 <joy.allen@newcastle.ac.uk>
2018-01-08 14:54:39 +0000

    Updates to analysis files.  Not commiting the data

---
+++ analysis.R
@@ -0,0 +1,56 @@
+# working script to analysis audit data
+
+library(ggplot2)
+library(plotly)
+packageVersion('plotly')
+
+rvi1516 <- read.csv("data/Audit1516.csv")
+rvi1617 <- read.csv("data/Audit1617.csv")
+rvi1718 <- read.csv("data/Audit1718.csv")
+
+rvi1516 <- as.data.frame(rvi1516)
+rvi1617 <- as.data.frame(rvi1617)
+rvi1718 <- as.data.frame(rvi1718)
+
+# combine years for numbers tested
+test <- merge(rvi1516, rvi1617, by = "Directorate", all.x = TRUE, all.y = TRUE)
+combined_years <- merge( test, rvi1718, by = "Directorate", all.x = TRUE, all.y = TRUE)
+
+num_tested <- cbind.data.frame(combined_years$Directorate, combined_years$No..tested.x,combined_years$No..tested.y, combined_years$No..tested)
+colnames(num_tested) <- c("Directorate", "num_tested1516", "num_tested1617", "num_tested1718")
+#rownames(num_tested) <- num_tested[,1]
+#num_tested[,1] <- NULL
+#num_tested[1,] <- NULL
+
+# plot numbers tested each year
+
+g <- plot_ly(num_tested, x = ~Directorate, y  = ~num_tested1516, type = 'bar', name = '15/16') %>%
+  add_trace(y = ~num_tested1617, name = '16/17') %>%
+  add_trace(y = ~num_tested1718, name = '17/18') %>%
+  layout(yaxis = list(title = 'Count'), barmode = 'group', title = "Number tested")
+g
+
+
+#  % carriage and % positivity
+percent_carry <- cbind.data.frame(combined_years$Directorate, combined_years$X.carrier.x,combined_years$X.carrier.y, combined_years$X.carrier)
+colnames(percent_carry) <- c("Directorate", "percent_carry1516", "percent_carry1617", "percent_carry1718")
+
+g <- plot_ly(percent_carry, x = ~Directorate, y  = ~percent_carry1516, type = 'bar', name = '15/16') %>%
+  add_trace(y = ~percent_carry1617, name = '16/17') %>%
+  add_trace(y = ~percent_carry1718, name = '17/18') %>%
+  layout(yaxis = list(title = 'Count'), barmode = 'group', title = "Percentage carriers")
+g
+
+percent_pos <- cbind.data.frame(combined_years$Directorate, combined_years$X..positive.x,combined_years$X..positive.y, combined_years$X..positive)
+colnames(percent_pos) <- c("Directorate", "percent_pos1516", "percent_pos1617", "percent_pos1718")
+
+g <- plot_ly(percent_pos, x = ~Directorate, y  = ~percent_pos1516, type = 'bar', name = '15/16') %>%
+  add_trace(y = ~percent_pos1617, name = '16/17') %>%
+  add_trace(y = ~percent_pos1718, name = '17/18') %>%
+  layout(yaxis = list(title = 'Count'), barmode = 'group', title = "Percentage positive")
+g
+
+
+
+
+
